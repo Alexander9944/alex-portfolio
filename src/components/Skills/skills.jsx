0092f50722fffb464d796c9a8e2e71c0692dda49
Caushy <angeljasper333@gmail.com>
2025-03-07 19:55:35 +0530

    skill logo changes

--- src/components/Skills/skills.jsx
+++ src/components/Skills/skills.jsx
@@ -1,4 +1,3 @@
-
 import './skills.css';
 import restapi from '../../assets/Rest-API-Large-1024x808.png';
 import css from '../../assets/icons8-css-96.png';
@@ -15,34 +14,28 @@ import jquery from '../../assets/icons8-jquery-100.png';
 import postgresql from '../../assets/icons8-postgresql-96.png';
 import wordpress from '../../assets/icons8-wordpress-96.png';
 
-
-
 const Skills = () => {
+  const icons = [restapi, js, css, react, node, mongodb, github, npm, html, express, bootstrap, jquery, postgresql, wordpress];
+
   return (
-    <div>
-    <section id='skills'>
+    <div className="skills-container">
+      <section id="skills">
         <span className="skillTitle">Skills</span>
-        <span className="skillDesc">I am a skilled and passionate web designer with experience in creating visually appealing and user friendly websites.I have strong understanding of design and a keen eye for detail.I am profecient in HTML, CSS,Javascript and React. </span>
-    </section>
-      <div>
-       <img src={restapi} alt="Profile" className="restapi" />
-        <img src={js} alt="Profile" className="js" />
-        <img src={css} alt="Profile" className="css" />
-        <img src={react} alt="Profile" className="react" />
-        <img src={node} alt="Profile" className="node" />
-        <img src={mongodb} alt="Profile" className="mongodb" />
-        <img src={github} alt="Profile" className="github" />
-        <img src={npm} alt="Profile" className="npm" />
-        <img src={html} alt="Profile" className="html" />
-        <img src={express} alt="Profile" className="express" />
-        <img src={bootstrap} alt="Profile" className="bootstrap" />
-        <img src={jquery} alt="Profile" className="jquery" />
-        <img src={postgresql} alt="Profile" className="postgresql" />
-        <img src={wordpress} alt="Profile" className="wordpress" />
+        <span className="skillDesc">
+          I am a skilled and passionate web designer with experience in creating <br></br>visually appealing and user-friendly websites. 
+          I have a strong understanding of design and a keen eye for detail. I am proficient in HTML, CSS, JavaScript, and React.
+        </span>
+      </section>
+
+      <div className="skills-slider">
+        <div className="skills-slide-track">
+          {icons.concat(icons).map((icon, index) => (
+            <img key={index} src={icon} alt="Skill" className="icon" />
+          ))}
+        </div>
       </div>
-      </div>
+    </div>
   );
-}
+};
 
 export default Skills;
-
